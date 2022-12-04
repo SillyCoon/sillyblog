b8fdd3c557883fb96e0e8d70d4e878bc282be50b
Aleksei Ivanov <aleksei.ivanov@rakuten.com>
2022-12-05 08:47:55 +0900

    mdx integration added

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()],
+  integrations: [preact(), tailwind(), mdx()]
 });
