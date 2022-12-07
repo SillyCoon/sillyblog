66706d73412bd38831f85a649dfd543af11c7d8c
Aleksei Ivanov <aleksei.ivanov@rakuten.com>
2022-12-07 11:52:11 +0900

    not working autolink headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 // https://astro.build/config
 import preact from '@astrojs/preact';
@@ -8,8 +9,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind(), mdx()]
+  integrations: [preact(), tailwind(), mdx(
+    {rehypePlugins: [rehypeAutolinkHeadings]}
+  )]
 });
